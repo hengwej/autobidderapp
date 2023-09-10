04afd7f04b3d0890578f4e2ad4da34268b0dc7a2
Darren <2100692@sit.singaporetech.edu.sg>
2023-09-10 17:21:11 +0800

    Housekeeping. Email and pwd doesn't work cos no Django

--- src/App.js
+++ src/App.js
@@ -5,14 +5,13 @@ import LoginButton from './components/LoginButton';
 import LogoutButton from './components/LogoutButton';
 import Profile from './components/Profile';
 
-const domain = process.env.REACT_APP_AUTH0_DOMAIN;
-const clientid = process.env.REACT_APP_AUTH0_CLIENT_ID;
+
 
 function App() {
   return (
 
         <main className="column">
-          <h1> AUth0 Login</h1>
+          <h1> Auth0 Login Test</h1>
           <LoginButton />
           <LogoutButton />
           <Profile />
