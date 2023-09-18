72b303f85b2d13d89dc5603db4a13090a2e05333
hengwej <jeanheng9@gmail.com>
2023-09-18 22:41:42 +0800

    Project base

---
+++ src/pages/index.js
@@ -0,0 +1,14 @@
+// pages/index.js
+
+import React from 'react';
+
+function HomePage() {
+    return (
+        <div>
+            <h1>Welcome to My Next.js App</h1>
+            <p>This is the homepage of my Next.js application.</p>
+        </div>
+    );
+}
+
+export default HomePage;
