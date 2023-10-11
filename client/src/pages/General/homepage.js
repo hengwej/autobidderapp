42d5ffe680b4938bad4465d23d7abc96f0f7e7d0
jodiesa <90232354+jodiesa@users.noreply.github.com>
2023-10-11 19:21:50 +0800

    homepage add

--- client/src/pages/General/homepage.js
+++ client/src/pages/General/homepage.js
@@ -1,50 +1,77 @@
-import React from "react";
+import React, { Component } from "react";
 import "../../css/styles.css";
 import Card from "react-bootstrap/Card";
 import Container from "react-bootstrap/Container";
 import Row from "react-bootstrap/Row";
 import Col from "react-bootstrap/Col";
+import Header from "../../components/Header";
 
-export default class Home extends React.Component {
+export default class Home extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            carData: [] // Initialize an empty array to store car data
+        };
+    }
+
+    // Simulated data fetching (replace with actual API call)
+    componentDidMount() {
+        // Simulate fetching car data from a database or API
+        const fetchedData = [
+            {
+                id: 1,
+                image: "car1.jpg",
+                price: "$25,000",
+                modelName: "Model X"
+            },
+            {
+                id: 2,
+                image: "car2.jpg",
+                price: "$30,000",
+                modelName: "Model S"
+            },
+            {
+                id: 3,
+                image: "car3.jpg",
+                price: "$20,000",
+                modelName: "Model 3"
+            }
+            // Add more car data here
+        ];
+
+        this.setState({ carData: fetchedData });
+    }
+
     render() {
         return (
-            <div class="navbar navbar-expand-lg navbar-light" id="mainNav">
-                <a style={{ position: "relative", right: 80 + 'px', bottom: 260 + 'px', fontSize: 25 + 'px', color: "white" }} class="navbar-brand" href="#page-top">Auctions</a>
-                <Container>
+            <div>
+                <a
+                    style={{ margin: "20px 20px 0",fontSize : "25px", color: "white" }}
+                    className="navbar-brand"
+                    href="#page-top"
+                >
+                    Auctions
+                </a>
+
+                <Header headerText="Welcome" />
+
+                <div className="navbar navbar-expand-lg navbar-light" id="mainNav"></div>
+                <Container fluid> {/* Use a fluid container for a full-width layout */}
                     <Row>
-                        <Col md={4}>
-                            <Card>
-                                <Card.Body>
-                                    <Card.Title>Card 1</Card.Title>
-                                    <Card.Text>
-                                        This is some text within Card 1.
-                                    </Card.Text>
-                                </Card.Body>
-                            </Card>
-                        </Col>
-                        <Col md={4}>
-                            <Card>
-                                <Card.Body>
-                                    <Card.Title>Card 2</Card.Title>
-                                    <Card.Text>
-                                        This is some text within Card 2.
-                                    </Card.Text>
-                                </Card.Body>
-                            </Card>
-                        </Col>
-                        <Col md={4}>
-                            <Card>
-                                <Card.Body>
-                                    <Card.Title>Card 3</Card.Title>
-                                    <Card.Text>
-                                        This is some text within Card 3.
-                                    </Card.Text>
-                                </Card.Body>
-                            </Card>
-                        </Col>
+                        {this.state.carData.map((car) => (
+                            <Col lg={4} key={car.id}> {/* Use lg for larger column widths */}
+                                <Card>
+                                    <Card.Img src={car.image} alt={car.modelName} />
+                                    <Card.Body>
+                                        <Card.Title>{car.modelName}</Card.Title>
+                                        <Card.Text>Price: {car.price}</Card.Text>
+                                    </Card.Body>
+                                </Card>
+                            </Col>
+                        ))}
                     </Row>
                 </Container>
             </div>
-        )
+        );
     }
 }
