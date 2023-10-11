093c48e0a3caef14f7562756e4892ceb349c0df3
hengwej <jeanheng9@gmail.com>
2023-10-11 17:17:42 +0800

    homepage structure

--- client/src/App.js
+++ client/src/App.js
@@ -1,17 +1,38 @@
-import React, { useEffect, useState } from 'react';
+import { Routes, Route, Link } from "react-router-dom";
+import Homepage from "./pages/General/homepage";
+import FAQ from "./pages/General/faq";
+import SellCar from "./pages/Users/sell_a_car";
+import "./css/styles.css";
 
 function App() {
-  const [message, setMessage] = useState('');
-  useEffect(() => {
-    fetch('http://localhost:5000')
-      .then((res) => res.text())
-      .then((data) => setMessage(data))
-      .catch((err) => console.log(err));
-  }, []);
-  return (
-    <div>
-      <h1>{message}</h1>
-    </div>
-  );
+    return (
+        <div className="App">
+            <nav class="navbar navbar-expand-lg navbar-light fixed-top" id="mainNav">
+                <div class="container px-4 px-lg-5">
+                    <a class="navbar-brand" href="#page-top" style={{ fontSize: 35 + 'px' }}>Auto Bid</a>
+                    <ul class="navbar-nav ms-auto">
+                        <li class="nav-item">
+                            <Link class="nav-link" to="/">Auctions</Link>
+                        </li>
+                        <li class="nav-item">
+                            <Link class="nav-link" to="/faq">FAQ</Link>
+                        </li>
+                        <li class="nav-item">
+                            <Link class="nav-link" to="/sellCar">Sell a Car</Link>
+                        </li>
+                        <li class="nav-item" style={{marginTop: 28 +'px'}}>
+                            <a class="btn btn-primary" href="#about">Login</a>
+                        </li>
+                    </ul>
+                </div>
+            </nav>
+            <Routes>
+                <Route path="/" element={<Homepage />} />
+                <Route path="/faq" element={<FAQ />} />
+                <Route path="/sellCar" element={<SellCar />} />
+            </Routes>
+        </div>
+    );
 }
+
 export default App;
---
+++ client/src/pages/General/faq.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+export default class FAQ extends React.Component {
+    render() {
+        return (
+            <div>
+                <p>FAQ</p>
+            </div>
+        )
+    }
+}
