ea53789542f6a409e1d6a64ef29a2bcd4e0b089d
chococtea <92836838+chococtea@users.noreply.github.com>
2023-10-11 13:28:56 +0800

    Fixed the datatyoes

--- mysql_db_schema/create_tables.sql
+++ mysql_db_schema/create_tables.sql
@@ -72,7 +72,7 @@ CREATE TABLE IF NOT EXISTS Orders (
     deliveryType VARCHAR(20) NOT NULL,
     shippingAddress VARCHAR(255),
     orderCreationTime DATETIME NOT NULL,
-    orderCompletionTime DateTime,
+    orderCompletionTime DATETIME,
     orderCreatorID INT,
     auctionID INT,
     PRIMARY KEY (orderID),
