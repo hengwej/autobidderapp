cdc5b8de58aa78a9e32f28ce9ba74f16079d0636
Zafranno161 <91773110+Zafranno161@users.noreply.github.com>
2023-10-17 21:59:56 +0800

    OTP confirmation page
    - Created OTP confirmation page
    - Fixed CSS Bug

--- client/src/App.js
+++ client/src/App.js
@@ -2,13 +2,14 @@ import React from "react";
 import { Routes, Route, Link } from "react-router-dom";
 import Homepage from "./pages/General/Homepage";
 import FAQ from "./pages/General/FAQ";
-import SignUp from "./pages/General/SignUp";
-import Login from "./pages/General/Login";
-import Logout from "./pages/General/Logout";
+import SignUp from "./pages/Auth/SignUp";
+import Login from "./pages/Auth/Login";
+import Logout from "./pages/Auth/Logout";
 import SellCar from "./pages/Users/SellACar/sell_a_car";
 import ViewCarDetails from "./pages/Users/CarDetails/view_car_details";
 import Placebid from "./pages/Users/PlaceBid/placebid";
 import "./css/styles.css";
+import Login2FA from "./pages/Auth/Login2FA";
 
 
 function App() {
@@ -17,7 +18,7 @@ function App() {
         <div className="App">
             <nav className="navbar navbar-expand-lg navbar-light fixed-top" id="mainNav">
                 <div className="container px-4 px-lg-5">
-                    <a className="navbar-brand" href="#page-top" style={{ fontSize: 35 + 'px' }}>Auto Bid</a>
+                    <a className="navbar-brand" href="/" style={{ fontSize: 35 + 'px' }}>Auto Bid</a>
                     <ul className="navbar-nav ms-auto">
                         <li className="nav-item">
                             <Link className="nav-link" to="/">Auctions</Link>
@@ -29,7 +30,7 @@ function App() {
                             <Link className="nav-link" to="/sellCar">Sell a Car</Link>
                         </li>
                         <li className="nav-item">
-                            <Link className="nav-link" to="/login">Login</Link>
+                            <Link className="nav-link" to="/auth/login">Login</Link>
                         </li>
                         <li className="nav-item">
                             <Link className="nav-link" to="/signup">Sign Up</Link>
@@ -44,7 +45,8 @@ function App() {
                 <Route path="/" element={<Homepage />} />
                 <Route path="/faq" element={<FAQ />} />
                 <Route path="/sellCar" element={<SellCar />} />
-                <Route path="/login" element={<Login />} />
+                <Route path="/auth/login" element={<Login />} />
+                <Route path="/auth/confirmation" element={<Login2FA />} />
                 <Route path="/logout" element={<Logout />} />
                 <Route path="/signup" element={<SignUp />} />
                 <Route path="/viewCarDetails" element={<ViewCarDetails />} />
--- client/src/pages/Auth/Login/index.js
+++ client/src/pages/Auth/Login/index.js
@@ -5,7 +5,7 @@ import * as Yup from 'yup';
 
 function Login() {
     const initialValues = {
-        email: '',
+        username: '',
         password: ''
     }
 
@@ -15,19 +15,20 @@ function Login() {
     }
 
     const validationSchema = Yup.object().shape({
-        email: Yup.string().email("Invalid email").required("You must enter an email"),
+        username: Yup.string().required("You must enter a username"),
         password: Yup.string().min(8).required("You must enter a password")
     });
 
     return (
         <div className='loginPage'>
+            <h3>Login</h3>
             <Formik initialValues={initialValues} onSubmit={onSubmit} validationSchema={validationSchema}>
                 <Form>
-                    <label>Email: </label>
-                    <Field id="inputLoginEmail" type="email" name="email" placeholder="Email" autoComplete="off" />
-                    <ErrorMessage className="error-message" name="email" component="span" />
+                    <label>Username: </label>
+                    <Field id="inputLoginUsername" type="text" name="username" placeholder="Username" />
+                    <ErrorMessage className="error-message" name="username" component="span" />
                     <label>Password: </label>
-                    <Field id="inputLoginPassword" type="password" name="password" placeholder="Password" autoComplete="off" />
+                    <Field id="inputLoginPassword" type="password" name="password" placeholder="Password" />
                     <ErrorMessage className="error-message" name="password" component="span" />
 
                     <button type="submit">Login</button>
