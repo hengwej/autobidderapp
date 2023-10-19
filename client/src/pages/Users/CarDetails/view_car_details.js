ddcf4e8d37918739bc0db961c11e1aad93875b4e
hengwej <jeanheng9@gmail.com>
2023-10-19 12:41:56 +0800

    updated view car details page

--- client/src/pages/Users/CarDetails/view_car_details.js
+++ client/src/pages/Users/CarDetails/view_car_details.js
@@ -1,7 +1,7 @@
 import React, { useEffect, useState } from "react";
-import "../../css/styles.css";
+import "./styles.css";
 import { Modal, Button } from "react-bootstrap";
-import Placebid from "./placebid";
+import Placebid from "../PlaceBid/placebid";
 import { useParams } from "react-router-dom";
 
 export default function ViewCarDetails() {
@@ -10,7 +10,8 @@ export default function ViewCarDetails() {
     const [timeLeft, setTimeLeft] = useState({});
     const [showPlaceBidModal, setShowPlaceBidModal] = useState(false);
     const [error, setError] = useState(null);
-    
+    let countdownInterval;
+
     const handlePlaceBidClose = () => {
         setShowPlaceBidModal(false);
     };
@@ -22,7 +23,7 @@ export default function ViewCarDetails() {
     useEffect(() => {
         async function fetchData() {
             try {
-                const response = await fetch("http://localhost:5000/");
+                const response = await fetch("http://127.0.0.1:5000/api/cars/allCar");
                 const data = await response.json();
                 console.log(data);
                 const car = data.find((car) => car.carID === parseInt(carID));
@@ -43,40 +44,96 @@ export default function ViewCarDetails() {
         }
 
         fetchData();
-    }, { carID });
+    }, [ carID ]);
+
+    useEffect(() => {
+        if (carData && carData.createdAt) {
+            calculateTimeLeft();
+            startCountdown();
+        }
+    }, [carData]);
+
+    //const calculateTimeLeft = () => {
+    //    const dateString = carData.createdAt;
+    //    console.log("date " + carData.createdAt);
+    //    const startDate = new Date(dateString);
+    //    const endDate = new Date(startDate);
+    //    endDate.setDate(endDate.getDate() + 7);
+    //    const currentDate = new Date();
+    //    const difference = endDate - startDate;
+    //    let timeLeft = {};
+
+    //    if (currentDate < startDate) {
+    //        console.log('The event has not started yet.');
+    //    } else if (currentDate >= startDate && currentDate <= endDate) {
+    //        timeLeft = {
+    //            days: Math.floor(difference / (1000 * 60 * 60 * 24)),
+    //            hours: Math.floor((difference / (1000 * 60 * 60)) % 24),
+    //            minutes: Math.floor((difference / (1000 * 60)) % 60),
+    //            seconds: Math.floor((difference / 1000) % 60)
+    //        };
+    //    }
+    //    else {
+    //        console.log('The event has ended.');
+    //    }
+    //    //if (difference >= 0) {
+            
+    //    //}
+
+    //    setTimeLeft(timeLeft);
+    //};
 
     const calculateTimeLeft = () => {
-        const year = new Date().getFullYear();
-        const difference = +new Date(`${year}-12-31`) - +new Date();
-        let timeLeft = {};
+        const dateString = carData.createdAt;
+        const startDate = new Date(dateString);
+        const endDate = new Date(startDate);
+        endDate.setDate(endDate.getDate() + 7);
+        const currentDate = new Date();
+        const difference = endDate - currentDate;
 
-        if (difference >= 0) {
-            timeLeft = {
-                /*days: Math.floor(difference / (1000 * 60 * 60 * 24)),*/
-                hours: Math.floor((difference / (1000 * 60 * 60)) % 24),
-                minutes: Math.floor((difference / (1000 * 60)) % 60),
-                seconds: Math.floor((difference / 1000) % 60)
-            };
+        if (difference > 0) {
+            const days = Math.floor(difference / (1000 * 60 * 60 * 24));
+            const hours = Math.floor((difference / (1000 * 60 * 60)) % 24);
+            const minutes = Math.floor((difference / (1000 * 60)) % 60);
+            const seconds = Math.floor((difference / 1000) % 60);
+
+            setTimeLeft({
+                days,
+                hours,
+                minutes,
+                seconds,
+            });
+        } else {
+            clearInterval(countdownInterval);
+            setTimeLeft({ days: 0, hours: 0, minutes: 0, seconds: 0 });
+            console.log('The event has ended.');
         }
-
-        setTimeLeft(timeLeft);
     };
 
     const startCountdown = () => {
-        this.countdownInterval = setInterval(() => {
+        countdownInterval = setInterval(() => {
             calculateTimeLeft();
         }, 1000);
     };
 
     return (
         <div class="wrapper">
+            <div key={carData.carID}>
+                <label class="cardetails_label">{carData.make}&nbsp;{carData.model}</label><br />
+                {carData && carData.carImage && carData.carImage.data && (
+                    <img src={URL.createObjectURL(new File([new Blob([new Uint8Array(carData.carImage.data)])], { type: 'image/jpeg' }))} class="carImages" />
+                )}
+            </div>
+
             <div class="timer">
-                Time Left:{" "}
+                <b>Time Left:</b>{" "}
                 {Object.keys(timeLeft).map((interval) => (
                     <span key={interval}>
                         {timeLeft[interval]} {interval}{" "}
                     </span>
                 ))}
+                <b>Last Bid ($)</b>
+                <span>500</span>
             </div>
 
             <Button variant="warning" onClick={handlePlaceBidShow} style={{ marginLeft: 568 + 'px', padding: 8 + 'px' }}>
@@ -98,19 +155,23 @@ export default function ViewCarDetails() {
 
             <table key={carData.carID}>
                 <tr>
-                    <th style={{ width: 180 + 'px', borderRight: "2px solid white" }}>Make</th>
-                    <td style={{ width: 500 + 'px' }}>{carData.make}</td>
+                    <th style={{ width: 180 + 'px', borderRight: "2px solid black" }}>Vehicle Number</th>
+                    <td style={{ width: 500 + 'px' }}>{carData.vehicleNumber}</td>
                 </tr>
                 <tr>
-                    <th style={{ borderRight: "2px solid white" }}>Model</th>
+                    <th style={{ width: 180 + 'px', borderRight: "2px solid black" }}>Make</th>
+                    <td>{carData.make}</td>
+                </tr>
+                <tr>
+                    <th style={{ borderRight: "2px solid black" }}>Model</th>
                     <td>{carData.model}</td>
                 </tr>
                 <tr>
-                    <th style={{ borderRight: "2px solid white" }}>Interior Color</th>
+                    <th style={{ borderRight: "2px solid black" }}>Interior Color</th>
                     <td>{carData.interiorColor}</td>
                 </tr>
                 <tr>
-                    <th style={{ borderRight: "2px solid white" }}>Exterior Color</th>
+                    <th style={{ borderRight: "2px solid black" }}>Exterior Color</th>
                     <td>{carData.exteriorColor}</td>
                 </tr>
             </table>
