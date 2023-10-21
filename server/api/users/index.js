169cf1c0a917f4fc99f146ef7a61d98fb45db53e
jodiesa <90232354+jodiesa@users.noreply.github.com>
2023-10-21 14:51:57 +0800

    View user admin feature fixed

--- server/api/users/index.js
+++ server/api/users/index.js
@@ -7,5 +7,8 @@ router.get('/getAllUsers', controller.getAllUsers);
 
 // Add a route for deleting a user by ID
 router.delete('/deleteUser/:id', controller.deleteUser);
+// Add a route for viewing a user by ID
+router.get('/viewUser/:userID', controller.viewUser);
+
 
 module.exports = router;
