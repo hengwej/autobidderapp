4640e61865d4dce1179c627b3fca595eadfce034
chococtea <92836838+chococtea@users.noreply.github.com>
2023-10-22 15:56:03 +0800

    Fixed Account's controller.js

--- server/api/accounts/controller.js
+++ server/api/accounts/controller.js
@@ -14,4 +14,51 @@ exports.allAccount = async (req, res) => {
     res.json(allAccounts);
 };
 
+exports.getUserAccountDetails = async (req, res) => {
+    const accountID = req.params.id; // Use req.params.id to get the account ID from the URL
 
+    try {
+        const account = await prisma.account.findUnique({
+            where: { accountID: parseInt(accountID) }, // Parse to an integer
+        });
+
+        if (!account) {
+            return res.status(404).json({ error: 'Account not found' });
+        }
+
+        res.json(account);
+    } catch (error) {
+        console.error("Error getting account details:", error);
+        res.status(500).json({ error: 'Internal server error' });
+    }
+};
+
+exports.deleteAccount = async (req, res) => {
+    const accountID = req.params.id; // Use req.params.id to get the account ID from the URL
+
+    try {
+        // Check if the account exists
+        const account = await prisma.account.findUnique({
+            where: { accountID: parseInt(accountID) }, // Parse to an integer
+            include: {
+                user: true, // Include the associated user
+            },
+        });
+
+        if (!account) {
+            return res.status(404).json({ error: 'Account not found' });
+        }
+
+        // Delete the user (if it exists), and the account will be deleted automatically due to the cascade delete relationship
+        if (account.user) {
+            await prisma.user.delete({
+                where: { userID: account.user.userID },
+            });
+        }
+
+        res.json({ message: 'Account and associated user deleted successfully' });
+    } catch (error) {
+        console.error("Error deleting account and user:", error);
+        res.status(500).json({ error: 'Internal server error' });
+    }
+};
