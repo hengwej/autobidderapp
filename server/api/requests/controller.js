f890f4433f8e38b6177ea7f1317ced8dc2966044
jodiesa <90232354+jodiesa@users.noreply.github.com>
2023-10-23 03:53:00 +0800

    Reject request button- Admin
    works!

--- server/api/requests/controller.js
+++ server/api/requests/controller.js
@@ -33,3 +33,35 @@ exports.viewRequestDetails = async (req, res) => {
         res.status(500).json({ error: 'Internal server error' });
     }
 };
+
+
+// Create a new controller function for deleting a request
+exports.deleteRequest = async (req, res) => {
+    try {
+        const requestID = parseInt(req.params.requestID);
+
+        if (isNaN(requestID)) {
+            return res.status(400).json({ error: 'Invalid request ID' });
+        }
+
+        // Check if the request exists before deletion
+        const existingRequest = await prisma.request.findUnique({
+            where: { requestID: requestID },
+        });
+
+        if (!existingRequest) {
+            return res.status(404).json({ error: 'Request not found' });
+        }
+
+        // Perform the deletion of the request
+        await prisma.request.delete({
+            where: { requestID: requestID },
+        });
+
+        // Return a success message
+        res.json({ message: 'Request deleted successfully' });
+    } catch (error) {
+        console.error('Error deleting request:', error);
+        res.status(500).json({ error: 'Internal server error' });
+    }
+};
