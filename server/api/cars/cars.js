751b119b5bde32e25394de2d6778cc860dd5167d
Zafranno161 <91773110+Zafranno161@users.noreply.github.com>
2023-10-23 17:39:55 +0800

    Added SellCar Page by EngChun

---
+++ server/api/cars/cars.js
@@ -0,0 +1,55 @@
+const express = require('express');
+const { PrismaClient } = require('@prisma/client');
+const jwt = require('jsonwebtoken');
+const router = express.Router();
+const prisma = new PrismaClient();
+
+
+router.post('/allCar', async (req, res) => {
+    const allCars = await prisma.car.findMany();
+    res.json(allCars);
+});
+
+router.post('/addCar', async (req, res) => {
+    const newCar = await prisma.car.create({
+        data: req.body,
+    });
+    res.json(newCar);
+});
+
+router.post('/sellCar', async (req, res) => {
+    const token = req.cookies.token;
+    console.log(req.body.formData);
+
+    if (!token) return res.status(401).json({ error: 'Unauthorized' });
+
+    try {
+        //Verify token
+        const payload = jwt.verify(token, process.env.JWT_SECRET);
+
+        //Perform Database calls here
+        //Find account associated with the token by accountID
+        const account = await prisma.account.findUnique({
+            where: {
+                accountID: payload.accountID, // Use accountID from token payload
+            },
+            //Add in include to add new car to account
+
+        });
+
+
+        //Return JSON object
+        //Return bidding history
+        res.status(200).json({ message: 'Car successfully sold' });
+
+    } catch (error) {
+        if (error.name === 'TokenExpiredError') {
+            return res.status(401).json({ error: 'Token has expired' });
+        }
+
+        console.error(error);
+        res.status(500).json({ error: 'Internal Server Error' });
+    }
+});
+
+module.exports = router;
--- server/api/cars/index.js
+++
@@ -1,8 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const controller = require('./controller');
-
-router.get('/allCar', controller.allCar);
-router.post('/addCar', controller.addCar);
-
-module.exports = router;
