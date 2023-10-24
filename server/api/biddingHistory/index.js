f9943797f27969851b0bc8b12cd8f4c867b8c4c2
hengwej <jeanheng9@gmail.com>
2023-10-24 23:14:45 +0800

    merge css

--- server/api/biddingHistory/index.js
+++ server/api/biddingHistory/index.js
@@ -3,6 +3,70 @@ const router = express.Router();
 const controller = require('./controller');
 
 router.get('/allBidHistory', controller.allBidHistory);
-router.post('/createBidHistory', controller.createBidHistory);
+
+router.post('/addBidHistory', async (req, res) => {
+    const token = req.cookies.token;
+    if (!token) return res.status(401).json({ error: 'Unauthorized' });
+
+    try {
+        //Verify token
+        const payload = jwt.verify(token, process.env.JWT_SECRET);
+
+        //Perform Database calls here
+        //Find account associated with the token by accountID
+        const existingBiddingHistory = await prisma.biddingHistory.findFirst({
+            where: {
+                accountID: payload.accountID, // Use accountID from token payload
+            },
+            include: {
+                auction: {
+                    include: {
+                        car: true,
+                    },
+                },
+            },
+        });
+
+        const { newBidHistory } = req.body;
+
+        if (existingBiddingHistory) {
+            // If a record with the accountID exists, update it
+            const updatedBiddingHistory = await prisma.biddingHistory.update({
+                where: {
+                    accountID: payload.accountID,
+                },
+                data: {
+                    // Update the fields you want to change
+                    // For example, update a 'bidValue' field
+                    bidAmount: rnewBidHistory.bidValue,
+                },
+            });
+            res.json(updatedBiddingHistory);
+        } else {
+            // If no record with the accountID exists, create a new record
+            const newBiddingHistory = await prisma.biddingHistory.create({
+                data: {
+                    accountID: payload.accountID,
+                    bidAmount: newBidHistory.bidValue,
+                    bidStatus: newBidHistory.status,
+                    auctionID: newBidHistory.auctionID,
+                },
+            });
+            res.json(newBiddingHistory);
+        }
+
+        //Return JSON object
+        //Return bidding history
+        res.json(biddingHistory);
+
+    } catch (error) {
+        if (error.name === 'TokenExpiredError') {
+            return res.status(401).json({ error: 'Token has expired' });
+        }
+
+        console.error(error);
+        res.status(500).json({ error: 'Internal Server Error' });
+    }
+});
 
 module.exports = router;
--- server/api/comments/index.js
+++ server/api/comments/index.js
@@ -1,7 +1,44 @@
 const express = require('express');
 const router = express.Router();
 const controller = require('./controller');
+const jwt = require('jsonwebtoken');
+const { PrismaClient } = require('@prisma/client');
+const prisma = new PrismaClient();
 
 router.get('/allComment', controller.allComment);
+
+router.post('/addComment', async (req, res) => {
+    console.log("access comment");
+    const token = req.cookies.token;
+    console.log("token" + token);
+    if (!token) return res.status(401).json({ error: 'Unauthorized' });
+
+    try {
+        //Verify token
+        const payload = jwt.verify(token, process.env.JWT_SECRET);
+
+        const { newComment } = req.body;
+        console.log("comment" + newComment);
+        //Perform Database calls here
+        //Find account associated with the token by accountID
+        const comment = await prisma.comment.create({
+            data: {
+                accountID: payload.accountID, // Use accountID from token payload
+                commentDetails: newComment.comment,
+                auctionID: newComment.auctionID,
+            }
+        });
+
+        res.json(comment);
+
+    } catch (error) {
+        if (error.name === 'TokenExpiredError') {
+            return res.status(401).json({ error: 'Token has expired' });
+        }
+
+        console.error(error);
+        res.status(500).json({ error: 'Internal Server Error' });
+    }
+});
 
 module.exports = router;
