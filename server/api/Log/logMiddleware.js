4fed6479e26473c079cc0ac94ea9a7db5da12827
Engch <engchun@live.com.sg>
2023-11-01 05:55:54 +0800

    Log with encryption at rest (unfeasible?)
    it seems unfeasible to implement encryption at rest as it means that every time there is a entry of log, the key and iv must be saved somewhere to for decryption to read the logs...
    
    added documentation on sellCars.js, log.js, logMiddleware.js

--- server/api/Log/logMiddleware.js
+++ server/api/Log/logMiddleware.js
@@ -1,20 +1,21 @@
 const { log, createLogWrapper } = require('./log');
 const jwt = require('jsonwebtoken');
 
+// Exporting a middleware function to handle logging and user identification
 module.exports = (req, res, next) => {
-    const token = req.cookies.token;
-    const { method, url, ip, headers, user } = req;
-    const userAgent = headers['user-agent'];
+    const token = req.cookies.token;  // Retrieve token from request cookies
+    const { method, url, ip, headers, user } = req;  // Destructure request properties
+    const userAgent = headers['user-agent'];  // Extract user-agent from headers
     let userId = "";
     if (!token) {
-        userId = user ? user.userID : "Guest";
+        userId = user ? user.userID : "Guest";  // Set userId to 'Guest' if token is absent
     } else {
         //Verify token
         const payload = jwt.verify(token, process.env.JWT_SECRET);
-        userId = user ? user.userID : payload.accountID;
+        userId = user ? user.userID : payload.accountID;  // Set userId from token payload or user object
     }
-    req.log = createLogWrapper(userId);  // Create a wrapped logger and attach to req
-    req.log.info(`Received a ${method} request for ${url} from ${ip} User-Agent: ${userAgent}`);
+    req.log = createLogWrapper(userId);  // Create a wrapped logger instance with userId and attach to request object
+    req.log.info(`Received a ${method} request for ${url} from ${ip} User-Agent: ${userAgent}`);  // Log the request details
     // log.info(`UserID: ${userId}, Received a ${method} request for ${url} from ${ip} User-Agent: ${userAgent}`);
-    next();
+    next();  // Proceed to the next middleware function in the stack
 };
