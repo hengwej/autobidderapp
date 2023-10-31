9bc99f4c67c559058bade3f07abd343868b1c84f
Zafranno161 <91773110+Zafranno161@users.noreply.github.com>
2023-10-31 17:50:51 +0800

    Fixed bidding history

--- client/src/utils/AuctionAPI.js
+++ client/src/utils/AuctionAPI.js
@@ -7,6 +7,50 @@ const api = axios.create({
     withCredentials: true,
 });
 
+export const addBid = async (bidValue, carID) => {
+    try {
+        const response = await api.post("/api/auctions/addBid",
+            { bidValue, carID },
+            {
+                headers: {
+                    'Content-Type': 'application/json'
+                }
+            });
+        return response;
+    } catch (error) {
+        console.error("Error Updating profile:", error);
+        throw error;
+    }
+};
+
+export const addOrder = async (requestData) => {
+    try {
+        const response = await api.put("/api/auctions/addOrder", requestData, {
+            headers: {
+                'Content-Type': 'application/json'
+            }
+        });
+        return response;
+    } catch (error) {
+        console.error("Error Updating Order:", error);
+        throw error;
+    }
+};
+
+export const addSellingHistory = async (requestData) => {
+    try {
+        const response = await api.post("/api/auctions/addSellingHistory", requestData, {
+            headers: {
+                'Content-Type': 'application/json'
+            }
+        });
+        return response;
+    } catch (error) {
+        console.error("Error Adding History:", error);
+        throw error;
+    }
+};
+
 export const getAllAuctions = async () => {
     try {
         const response = await api.post("/api/auctions/allAuction");
