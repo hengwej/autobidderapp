0b9e2e75a3ae06cdcb464efe228218987111aa5e
Painbow <mylittleporygon@gmail.com>
2023-11-03 01:35:01 +0800

    Update auctions.test.js

--- server/__tests__/auctions.test.js
+++ server/__tests__/auctions.test.js
@@ -1,3 +1,12 @@
+import axios from 'axios';
+
+const BASE_URL = process.env.REACT_APP_API_URL;
+
+const api = axios.create({
+    baseURL: BASE_URL,
+    withCredentials: true,
+});
+
 const request = require('supertest');
 const express = require('express');
 const auctionsRouter = require('../api/auctions/auctions'); // Update the path accordingly
@@ -7,10 +16,9 @@ app.use(express.json());
 app.use('/api/auctions', auctionsRouter);
 
 describe('Auctions API Endpoints', () => {
-    test('GET /api/auctions/allAuction should return all auctions', async() => {
-        const response = await request(app).post('/api/auctions/allAuction');
+    test('POST /api/auctions/allAuction should return all auctions', async() => {
+        const response = await api.post("/api/auctions/allAuction");
         expect(response.status).toBe(200);
-        //expect(response.body).toHaveLength(2); // Update the expected length according to your test data
     });
 
     // Add more test cases for other routes as needed
