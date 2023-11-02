3da1d73e5790834e20fce268596787e960bc03fc
Painbow <mylittleporygon@gmail.com>
2023-11-03 01:51:05 +0800

    Update auctions.test.js

--- server/__tests__/auctions.test.js
+++ server/__tests__/auctions.test.js
@@ -8,7 +8,7 @@ app.use('/api/auctions', auctionsRouter);
 
 describe('Auctions API Endpoints', () => {
     test('POST /api/auctions/allAuction should return all auctions', async() => {
-        const response = await request(app).post('/api/auctions/allAuction');
+        const response = await request(app).post('localhost:5000/api/auctions/allAuction');
         expect(response.status).toBe(200);
         //expect(response.body).toHaveLength(2); // Update the expected length according to your test data
     });
