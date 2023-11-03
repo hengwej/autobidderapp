75f3e339822970dc75272f4b4515a138be65d4b6
Max <92162946+Painbow@users.noreply.github.com>
2023-11-03 07:47:51 -0400

    Fix auctions.test

--- server/__tests__/auctions.test.js
+++ server/__tests__/auctions.test.js
@@ -1,18 +1,32 @@
+
 const request = require('supertest');
 const express = require('express');
 const auctionsRouter = require('../api/auctions/auctions'); // Update the path accordingly
+const logMiddleware = require('../api/Log/logMiddleware');
+const { log, createLogWrapper } = require('../api/Log/log');
+
 
 const app = express();
+
+const context = {
+    userId: 'G', // You can provide the user ID as needed
+    method: 'POST', // The HTTP method
+    url: '/api/auctions/allAuction', // The route URL
+    ip: '127.0.0.1', // IP address
+    userAgent: 'Test User Agent', // User-Agent header
+};
+
+app.use((req, res, next) => {
+    req.log = createLogWrapper(context);
+    next();
+});
+ 
 app.use(express.json());
 app.use('/api/auctions', auctionsRouter);
-
-// Set the base URL for your requests
-const baseUrl = 'http://localhost:5000';
 
 describe('Auctions API Endpoints', () => {
     test('POST /api/auctions/allAuction should return all auctions', async() => {
         const response = await request(app).post('/api/auctions/allAuction')
-            .set('Host', baseUrl); // Set the base URL
 
         console.log(response.body);
         expect(response.status).toBe(200);
