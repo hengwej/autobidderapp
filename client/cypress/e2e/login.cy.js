2492028497517753bfa268c797e1a7003d0d0cd5
Max <92162946+Painbow@users.noreply.github.com>
2023-11-04 06:16:08 -0400

    Cypress test

--- client/cypress/e2e/login.cy.js
+++ client/cypress/e2e/login.cy.js
@@ -6,21 +6,30 @@ describe('Login Form Tests', () => {
     // Fill in other form fields
     cy.get('[data-testid=inputLoginUsername]').type('byleft555');
     cy.get('[data-testid=inputLoginPassword]').type('byleft555');
-    cy.wait(1000);
+    cy.wait(500);
 
     cy.get('[type=submit]').should('not.be.disabled');
-
-    cy.setCookie('tempToken', 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJhY2NvdW50SUQiOjcsIm90cCI6Ijc2ODE4MCIsInRpbWVzdGFtcCI6IjIwMjMtMTEtMDRUMDc6MDA6NTEuNjA2WiIsImlhdCI6MTY5OTA4MTI1MSwiZXhwIjoxNjk5MDgxODUxfQ.OKvj0O37urPvTj42Kbt5MUdUqQyDDToWIEwbL6jue_I')
 
     // Submit the form
     cy.get('[type=submit]').click(); // Submit button selector may vary
 
-    // Assert the desired behavior
-    // ...
+    cy.wait(500);
+
     cy.url().should('include', '/auth/confirmation');
-    /* ==== Generated with Cypress Studio ==== */
+    
+    cy.getCookie('tempToken').should('exist');
+
     cy.get('#inputCode2FA').type('123456');
     cy.get('button').click();
-    /* ==== End Cypress Studio ==== */
+  
+    cy.contains('Logout').should('be.visible');
+    cy.contains('User Profile').should('be.visible');
+
+    cy.get(':nth-child(4) > .nav-link').click();
+
+    cy.contains('Login').should('be.visible');
+    cy.contains('Sign Up').should('be.visible');
   });
+
+  
 });
