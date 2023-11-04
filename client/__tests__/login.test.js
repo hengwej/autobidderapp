2a63efaade516bddaf8180eea3d1dc221fe19efe
Max <92162946+Painbow@users.noreply.github.com>
2023-11-04 03:02:25 -0400

    Cypress UI testing working (for Login)

--- client/__tests__/login.test.js
+++ client/__tests__/login.test.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { render, screen } from '@testing-library/react';
+import { render, screen, act} from '@testing-library/react';
 import userEvent from '@testing-library/user-event';
 import '@testing-library/jest-dom';
 import { MemoryRouter } from 'react-router-dom'; // Import MemoryRouter
@@ -9,7 +9,7 @@ import Login from '../src/pages/Auth/Login/index.js';
 jest.mock('../src/utils/AuthProvider', () => {
     return {
         useAuth: () => ({
-            login: jest.fn(),
+            login: jest.fn(() => ({ status: 200 })),
             user: null,
         }),
     };
@@ -17,12 +17,10 @@ jest.mock('../src/utils/AuthProvider', () => {
 
 describe('Login Form Tests', () => {
     test('Test if Login Form renders correctly', () => {
-        render( <
-            MemoryRouter >
-            <
-            Login / >
-            <
-            /MemoryRouter>
+        render(
+            <MemoryRouter>
+                <Login />npm
+            </MemoryRouter>
         );
 
         const usernameLabel = screen.getByText(/Username:/i);
@@ -32,6 +30,35 @@ describe('Login Form Tests', () => {
         expect(usernameLabel).toBeInTheDocument();
         expect(passwordLabel).toBeInTheDocument();
         expect(loginButton).toBeInTheDocument();
+        expect(loginButton).toBeDisabled();
     });
-    
+
+    test('validates the login form with valid data', async () => {
+        render(
+            <MemoryRouter>
+                <Login />
+            </MemoryRouter>
+        );
+
+        // Use a more specific query for the button element
+        const usernameField = screen.getByTestId('inputLoginUsername')
+        const passwordField = screen.getByTestId('inputLoginPassword');
+        const loginButton = screen.getByRole('button', { name: /Login/i });
+        const recaptchaRef = screen.getByTestId('reCAPTCHA');
+        
+        await act( async() => {
+            // Try submitting the form without filling in any fields
+            userEvent.type(usernameField, 'byleft555');
+            userEvent.type(passwordField, 'byleft555');
+            userEvent.click(loginButton);
+        });
+
+        await act( async() => {
+            // Trigger onChange handler manually to enable the button
+            recaptchaRef.props.onChange('mock-recaptcha-value');
+        });
+        
+        expect(loginButton).toBeEnabled();
+        //expect(window.location.pathname).toBe('/auth/confirmation');
+    });
 });
